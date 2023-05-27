387a151bd183f2db6fbaa605a1047cbaec44ff0e
edem4963 <112492837+edem4963@users.noreply.github.com>
2023-05-27 16:52:59 -0400

    fix blog router and new post saved to user name

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,8 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 const logger = require('../utils/loggers')
+
 
 blogsRouter.get('/', async (request, response) => {
     const blogs =  await Blog.find({})
@@ -9,16 +11,21 @@ blogsRouter.get('/', async (request, response) => {
   
   blogsRouter.post('/', async (request, response) => {
     const body = request.body
+    const user = await User.findById(body.userId)
 
     const blog = new Blog({
       title: body.title,
       author: body.author,
       url: body.url,
       likes: body.likes,
+      user: user.id 
      
     })
   
-    const savedBlog = blog.save()
+    const savedBlog =  await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
     response.status(201).json(savedBlog)
   })
 
--- models/blog.js
+++ models/blog.js
@@ -5,10 +5,7 @@ const blogSchema = new mongoose.Schema({
     author: String,
     url: String,
     likes: Number,
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User'
-    }
+    user: String,
   })
 
   blogSchema.set('toJSON', {
