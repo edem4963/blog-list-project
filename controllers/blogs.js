b4984d520f03d614386ce204e310c98810c2969d
edem4963 <112492837+edem4963@users.noreply.github.com>
2023-05-30 18:06:32 -0400

    add token authorization for posting blog with username and password

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,6 +3,15 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const logger = require('../utils/loggers')
 
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
     const blogs =  await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -11,14 +20,19 @@ blogsRouter.get('/', async (request, response) => {
   
   blogsRouter.post('/', async (request, response) => {
     const body = request.body
-    const user = await User.findById(body.userId)
+
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+      if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
       title: body.title,
       author: body.author,
       url: body.url,
       likes: body.likes,
-      user: user.id 
+      user: user._id 
      
     })
   
